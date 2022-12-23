959bfad7eda6c3a47db221e0bcbcafef79730107
gvjacob <gvjacob@outlook.com>
2022-12-23 15:06:41 -0500

    chore: fix components eslint rules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,11 @@
-import Meta from 'components/Meta'
-import Image from 'next/image'
+import Meta from 'components/Meta';
 
-const Home = () => {
+export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Meta title="Hanvoice" />
 
-      <h1 className="text-6xl font-bold">
-        Hanvoice
-      </h1>
+      <h1 className="text-6xl font-bold">Hanvoice</h1>
     </div>
-  )
+  );
 }
-
-export default Home
