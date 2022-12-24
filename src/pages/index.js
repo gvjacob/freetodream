5b4f66d76574fca0fb79fedf9ca66d3f5f1cc44b
gvjacob <gvjacob@outlook.com>
2022-12-23 23:59:46 -0500

    feat: hero wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,28 @@
+import Image from 'next/image';
+
 import Meta from '@components/Meta';
 import { useLocaleTranslation } from '@utils/i18n';
+import hanvoiceHero from '@img/hanvoice-hero.jpg';
 
 export default function Home() {
   const lt = useLocaleTranslation();
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Meta title="Home | Hanvoice" />
 
-      <h1 className="text-6xl font-bold">Hanvoice</h1>
-      <p>{lt('navigation.about-hanvoice')}</p>
+      <header className="flex items-center justify-between mt-20 px-8 space-x-10">
+        <div>
+          <h1 className="text-6xl text-blue mb-8">{lt('home.headline')}</h1>
+          <p className="max-w-xs">{lt('home.introduction')}</p>
+        </div>
+
+        <Image
+          className="rounded-md w-1/2"
+          src={hanvoiceHero}
+          alt="North-Korean father and son sit beside each other."
+        />
+      </header>
     </div>
   );
 }
