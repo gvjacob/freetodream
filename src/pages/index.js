36dfd3995cd088b9fec69ec1e626d92a77be3244
gvjacob <gvjacob@outlook.com>
2022-12-25 18:29:48 -0800

    fix: tablet styling for homepage topper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import cn from 'classnames';
 import Image from 'next/image';
 
 import Meta from '@components/Meta';
@@ -10,6 +11,32 @@ export default function Home() {
   return (
     <div>
       <Meta title="Home | Hanvoice" />
+
+      <header
+        className={cn(
+          'flex flex-col-reverse px-4 mt-4',
+          'sm:flex-row sm:items-center sm:justify-between sm:mt-20 sm:px-8 sm:space-x-10',
+        )}
+      >
+        <div>
+          <h1
+            className={cn(
+              'text-blue text-3xl mb-4 mt-4',
+              'sm:text-4xl sm:mb-4',
+              'lg:text-6xl lg:mb-8',
+            )}
+          >
+            {lt('home.headline')}
+          </h1>
+          <p className="max-w-xs">{lt('home.introduction')}</p>
+        </div>
+
+        <Image
+          className="rounded-md max-sm:w-full w-1/2"
+          src={hanvoiceHero}
+          alt="North-Korean father and son sit beside each other."
+        />
+      </header>
     </div>
   );
 }
