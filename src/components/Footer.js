fb2959552f565fcf1f5835ece1dc95dcf281803d
gvjacob <gvjacob@outlook.com>
2022-12-29 06:47:07 +0800

    feat: translate footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,24 @@
 import SVG from '@components/SVG';
+import { useLocaleTranslation } from '@utils/i18n';
 import Link from 'next/link';
 
 export default function Footer() {
+  const lt = useLocaleTranslation();
+
   return (
     <footer className="hv-site-padding bg-white">
-      <Link href="/about">About Hanvoice</Link>
-      <Link href="/process">Process</Link>
+      <ul>
+        <li>
+          <Link href="/about">{lt('navigation.about-hanvoice')}</Link>
+        </li>
+        <li>
+          <Link href="/process">{lt('navigation.process')}</Link>
+        </li>
+      </ul>
 
-      <div>
-        <SVG.CanadaWordmark aria-hidden />
-        <span class="sr-only">Sponsored by the government of Canada.</span>
-      </div>
+      <SVG.CanadaWordmark aria-hidden />
 
-      <p>Program by HanVoice in collaboration with the Government of Canada</p>
+      <p>{lt('footer.program-credits')}</p>
 
       <p>© 2022 HanVoice.ca</p>
     </footer>
