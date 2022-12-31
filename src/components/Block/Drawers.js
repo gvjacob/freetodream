ffc105582f0f66aedde526941271f1a33d953065
gvjacob <gvjacob@outlook.com>
2023-01-01 07:41:44 +0800

    fix: adjust cross spacing

--- src/components/Block/Drawers.js
+++ src/components/Block/Drawers.js
@@ -6,15 +6,15 @@ export default function Drawers({ title, items }) {
     <section className="py-2">
       <h3 className="mb-2 text-xs uppercase">{title}</h3>
 
-      <Accordion.Root type="multiple">
+      <Accordion.Root type="single" collapsible>
         {items.map(({ header, content }, i) => (
           <Accordion.Item className="border-t-[1px] border-black py-2" value={`item-${i}`} key={i}>
-            <Accordion.Header className="flex justify-between items-center group">
-              <Accordion.Trigger className="font-semibold">{header}</Accordion.Trigger>
+            <Accordion.Header className="flex justify-between items-start group">
+              <Accordion.Trigger className="font-semibold text-left">{header}</Accordion.Trigger>
 
               <div
                 className={cn(
-                  'relative w-3 aspect-square',
+                  'relative w-3 aspect-square mt-1.5 flex-shrink-0 ml-1',
                   'before:absolute before:top-1/2 before:w-full before:border-b-[2px] before:border-black',
                   'after:absolute after:top-1/2 after:w-full after:border-b-[2px] after:border-black after:rotate-90',
                   'after:transition-transform group-[[data-state="open"]]:after:rotate-0',
