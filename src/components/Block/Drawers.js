3219e1f77163ca57e72c2c72c98ed581075ad71b
gvjacob <gvjacob@outlook.com>
2023-01-01 22:18:25 +0800

    fix: button for drawers

--- src/components/Block/Drawers.js
+++ src/components/Block/Drawers.js
@@ -9,20 +9,26 @@ export default function Drawers({ title, items }) {
       <Accordion.Root type="single" collapsible>
         {items.map(({ header, content }, i) => (
           <Accordion.Item className="border-t-[1px] border-black py-2" value={`item-${i}`} key={i}>
-            <Accordion.Header className="flex justify-between items-start group">
-              <Accordion.Trigger className="font-semibold text-left transition-colors hocus:text-gray-500">
-                {header}
-              </Accordion.Trigger>
-
-              <div
+            <Accordion.Header>
+              <Accordion.Trigger
                 className={cn(
-                  'relative w-3 aspect-square mt-1.5 flex-shrink-0 ml-1',
-                  'before:absolute before:top-1/2 before:w-full before:border-b-[2px] before:border-black',
-                  'after:absolute after:top-1/2 after:w-full after:border-b-[2px] after:border-black after:rotate-90',
-                  'after:transition-transform group-[[data-state="open"]]:after:rotate-0',
+                  'w-full flex justify-between items-start',
+                  'font-semibold text-left',
+                  'group transition-colors hocus:text-gray-500',
                 )}
-                aria-hidden
-              />
+              >
+                {header}
+
+                <div
+                  className={cn(
+                    'relative w-3 aspect-square mt-1.5 flex-shrink-0 ml-1',
+                    'before:absolute before:top-1/2 before:w-full before:border-b-[2px] before:border-black',
+                    'after:absolute after:top-1/2 after:w-full after:border-b-[2px] after:border-black after:rotate-90',
+                    'after:transition-transform group-[[data-state="open"]]:after:rotate-0',
+                  )}
+                  aria-hidden
+                />
+              </Accordion.Trigger>
             </Accordion.Header>
 
             <Accordion.Content className="py-2 mb-4">{content}</Accordion.Content>
