e6c0d5eacc62744a391702d541fd4c51d39d48a9
gvjacob <gvjacob@outlook.com>
2023-01-01 17:56:54 +0800

    feat: blue topper for articles

--- src/components/ArticleLayout.js
+++ src/components/ArticleLayout.js
@@ -4,13 +4,13 @@ import ArticleNavigation from '@components/ArticleNavigation';
 
 export default function ArticleLayout({ title, navigation, children }) {
   return (
-    <article className="hv-site-padding">
+    <article>
       <Meta title={`${title} | Hanvoice`} />
 
-      <header className="grid grid-cols-12 gap-4 mt-24 mb-8">
+      <header className="hv-site-padding grid grid-cols-12 gap-4 pt-24 pb-6 bg-blue mb-8">
         <h1
           className={cn(
-            'col-start-1 col-end-12 text-5xl',
+            'col-start-1 col-end-12 text-5xl text-white font-bold',
             'md:col-start-2 md:col-end-10 md:text-7xl',
           )}
         >
@@ -18,7 +18,7 @@ export default function ArticleLayout({ title, navigation, children }) {
         </h1>
       </header>
 
-      <div className={cn('grid grid-cols-12 grid-rows-1 gap-4')}>
+      <div className="hv-site-padding grid grid-cols-12 grid-rows-1 gap-4">
         <div className="h-full sm:col-start-11 md:col-start-9 row-start-1 col-span-full">
           <ArticleNavigation className="sticky top-20" items={navigation} />
         </div>
