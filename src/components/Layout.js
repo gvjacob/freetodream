f5ffeebdccd072689efc46cade3802f876c6ec32
gvjacob <gvjacob@outlook.com>
2023-01-03 00:10:54 +0800

    Favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,16 @@
+import Head from 'next/head';
 import Navigation from '@components/Navigation';
 import Footer from '@components/Footer';
 
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <meta name="theme-color" content="#2158F4" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      </Head>
+
       <Navigation />
       <main>{children}</main>
       <Footer />
