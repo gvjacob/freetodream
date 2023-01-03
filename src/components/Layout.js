a22f2389691c324f0f69c4db0569f5f3f0942a85
gvjacob <gvjacob@outlook.com>
2023-01-03 08:41:11 +0800

    feat: favicon updates and og image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+
+import Meta from '@components/Meta';
 import Navigation from '@components/Navigation';
 import Footer from '@components/Footer';
 
@@ -6,10 +8,24 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <meta name="theme-color" content="#2158F4" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <meta name="theme-color" content="#2158F4" key="theme-color" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+          key="favicon:small-icon"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+          key="favicon:large-icon"
+        />
       </Head>
+
+      <Meta description="" image="https://freetodream.ca/img/hanvoice-og.jpg" />
 
       <Navigation />
       <main>{children}</main>
