15490d3b036f6b717fbbc8977197e9e0d647fdcb
gvjacob <gvjacob@outlook.com>
2023-01-03 21:26:25 +0800

    Fix: print for callout block

--- src/components/Block/Callout.js
+++ src/components/Block/Callout.js
@@ -1,3 +1,9 @@
+import cn from 'classnames';
+
 export default function Callout({ children }) {
-  return <div className="bg-iron p-6 rounded-md text-sm">{children}</div>;
+  return (
+    <div className={cn('bg-iron p-6 rounded-md text-sm', 'print:border-[1px] print:border-black')}>
+      {children}
+    </div>
+  );
 }
