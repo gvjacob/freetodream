8dcd1479c57b23bc5b848c17b74a9a4be1cf7b2d
gvjacob <gvjacob@outlook.com>
2023-01-25 22:42:34 -0500

    fix: update callout color

--- src/components/Block/Callout.js
+++ src/components/Block/Callout.js
@@ -2,7 +2,12 @@ import cn from 'classnames';
 
 export default function Callout({ children }) {
   return (
-    <div className={cn('bg-iron p-6 rounded-md text-sm', 'print:border-[1px] print:border-black')}>
+    <div
+      className={cn(
+        'bg-blue-100 text-white p-6 rounded-md text-sm',
+        'print:border-[1px] print:border-black',
+      )}
+    >
       {children}
     </div>
   );
