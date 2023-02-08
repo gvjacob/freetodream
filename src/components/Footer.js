f3b54783fbaf29d3f3aa41538972378794a48c30
gvjacob <gvjacob@outlook.com>
2023-02-07 20:18:16 -0500

    style: adjust spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ export default function Footer() {
   const lt = useLocaleTranslation();
 
   return (
-    <footer className="mt-auto bg-blue">
-      <div className="hv-site-padding mx-auto 2xl:container">
-        <div className="mx-auto flex max-w-xl flex-col items-center gap-6 py-10 text-white lg:py-14">
+    <footer className="mt-auto bg-blue text-white">
+      <div className="hv-site-padding mx-auto pt-10 pb-8 lg:pt-14 lg:pb-8 2xl:container">
+        <div className="mx-auto mb-8 flex max-w-xl flex-col items-center gap-6">
           <h2 className="text-center text-3xl tracking-tight sm:text-4xl xl:text-6xl">
             {lt('footer.headline')}
           </h2>
@@ -21,10 +21,12 @@ export default function Footer() {
           </a>
         </div>
 
-        <div className="grid print:flex print:flex-col max-sm:grid-rows-2 max-sm:gap-10 sm:grid-cols-2 sm:gap-2">
-          <div>
-            <SVG.CanadaWordmark className="w-32" aria-hidden />
-            <p className="mt-4 max-w-sm">{lt('footer.program-credits')}</p>
+        <div className="flex items-end justify-between">
+          <div className="grid print:flex print:flex-col max-sm:grid-rows-2 max-sm:gap-10 sm:grid-cols-2 sm:gap-2">
+            <div>
+              <SVG.CanadaWordmark className="w-32" aria-hidden />
+              <p className="mt-4 max-w-sm">{lt('footer.program-credits')}</p>
+            </div>
           </div>
 
           <ul className="space-y-2 print:hidden">
@@ -38,9 +40,11 @@ export default function Footer() {
               <Link href="/life-in-canada">{lt('navigation.life-in-canada')}</Link>
             </li>
           </ul>
-        </div>
 
-        <p className="mt-10 print:mt-6">© 2022 HanVoice.ca</p>
+          <div>
+            <p className="mt-10 print:mt-6">© 2022 HanVoice.ca</p>
+          </div>
+        </div>
       </div>
     </footer>
   );
