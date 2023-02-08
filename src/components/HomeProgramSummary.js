c1743645cf747f9d522636a290b9713c6d33ebfc
gvjacob <gvjacob@outlook.com>
2023-02-08 11:08:13 -0500

    feat: program summary

---
+++ src/components/HomeProgramSummary.js
@@ -0,0 +1,43 @@
+import { useLocaleTranslation } from '@utils/i18n';
+import Container from '@components/Container';
+
+function SummarySection({ title, description }) {
+  return (
+    <div className="px-8">
+      <h3 className="mb-2 text-xl font-bold text-blue">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+export default function HomeProgramSummary() {
+  const lt = useLocaleTranslation();
+
+  return (
+    <Container>
+      <section
+        className="rounded-lg border border-gray-300 bg-white"
+        aria-label={lt('home.summary.heading')}
+      >
+        <h2 className="sr-only">{lt('home.summary.heading')}</h2>
+
+        <div className="grid grid-cols-3 divide-x-[1px] divide-gray-200 py-8">
+          <SummarySection
+            title={lt('home.summary.about')}
+            description={lt('home.summary.about-description')}
+          />
+
+          <SummarySection
+            title={lt('home.summary.eligibility')}
+            description={lt('home.summary.eligibility-description')}
+          />
+
+          <SummarySection
+            title={lt('home.summary.process')}
+            description={lt('home.summary.process-description')}
+          />
+        </div>
+      </section>
+    </Container>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Meta from '@components/Meta';
 import HomeTopper from '@components/HomeTopper';
+import HomeProgramSummary from '@components/HomeProgramSummary';
 import { useLocaleTranslation } from '@utils/i18n';
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <>
       <Meta title={`${lt('navigation.home')} — Free to Dream`} />
       <HomeTopper />
+      <HomeProgramSummary />
     </>
   );
 }
