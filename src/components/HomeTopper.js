6444dc9a19e48c8307f5bbdf0612c2e0ebef3344
gvjacob <gvjacob@outlook.com>
2023-02-09 20:48:28 -0500

    feat: play button on hero

--- src/components/HomeTopper.js
+++ src/components/HomeTopper.js
@@ -46,13 +46,31 @@ export default function HomeTopper() {
 
         <div className="w-1/2 flex-shrink-0 max-md:w-full">
           <Dialog.Root>
-            <Dialog.Trigger>
+            <div className="relative w-full">
               <Image
-                className="w-full rounded-md"
+                className="inline w-full rounded-md"
                 src={hanvoiceHero}
-                alt="North-Korean father and son sit beside each other."
+                alt="Sam, North Korean man in white t-shirt, looks out in the distance."
               />
-            </Dialog.Trigger>
+              <Dialog.Trigger className="group absolute inset-0">
+                <span className="sr-only">Listen to Sam's refugee story</span>
+
+                <div className="absolute bottom-0 right-0 flex aspect-square w-[10%] -translate-x-4 -translate-y-4 items-center justify-center rounded-full bg-blue text-white transition-colors group-hover:bg-blue-dark">
+                  <svg
+                    className="h-1/2 w-1/2 translate-x-[2px] transition-transform group-hover:scale-110"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </Dialog.Trigger>
+            </div>
 
             <Dialog.Portal>
               <Dialog.Overlay className="fixed inset-0 z-50 bg-black opacity-80" />
