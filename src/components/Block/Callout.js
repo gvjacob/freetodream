10c40ad994c8d05debda9f59feb9e1acd6c17b72
gvjacob <gvjacob@outlook.com>
2023-02-10 09:45:27 -0500

    style: adjust size for large screens

--- src/components/Block/Callout.js
+++ src/components/Block/Callout.js
@@ -4,7 +4,7 @@ export default function Callout({ children }) {
   return (
     <div
       className={cn(
-        'bg-blue-100 text-white p-6 rounded-md text-sm',
+        'rounded-md bg-blue-100 p-6 text-sm text-white xl:text-lg',
         'print:border-[1px] print:border-black',
       )}
     >
